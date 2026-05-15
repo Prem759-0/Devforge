f8bc57575e4fd36a46b7721aa65b3dd04c6a4dbb
prem-web.dev <a70064182@gmail.com>
2026-05-15 23:56:27 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,17 +8,6 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizeCss: true,
   },
-  headers: async () => [
-    {
-      source: '/:path*',
-      headers: [
-        { key: 'X-DNS-Prefetch-Control', value: 'on' },
-        { key: 'X-Frame-Options', value: 'DENY' },
-        { key: 'X-Content-Type-Options', value: 'nosniff' },
-        { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-      ],
-    },
-  ],
 };
 
 export default nextConfig;
